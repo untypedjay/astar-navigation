b796072f3a09cefe4b70fc2d8062e09603b88ddd
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-05-08 23:55:02 +0200

    Add Vertex clas

--- src/swe4/entities/Vertex.java
+++ src/swe4/entities/Vertex.java
@@ -1,4 +1,27 @@
 package swe4.entities;
 
 public class Vertex {
+  private double x;
+  private double y;
+
+  public Vertex(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public void setX(double x) {
+    this.x = x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public void setY(double y) {
+    this.y = y;
+  }
 }
