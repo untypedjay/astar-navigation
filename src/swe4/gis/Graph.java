6bc3c58331d3e89b8190cd705665ca87eba3edae
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-05-09 23:52:28 +0200

    Change interface of addVertex()

--- src/swe4/gis/Graph.java
+++ src/swe4/gis/Graph.java
@@ -5,27 +5,30 @@ import swe4.entities.Vertex;
 import swe4.exceptions.InvalidVertexIdException;
 
 import java.util.Collection;
+import java.util.UUID;
 
 public class Graph {
-  public int addVertex(double longitude, double latitude) {
-    return 0; // id of new vertex
+  public long addVertex(double longitude, double latitude) {
+
+    long id = UUID.randomUUID().toString().hashCode();
+    return id;
   }
 
   public void addEdge(String name, long startVertexId, long endVertexId, double length) throws InvalidVertexIdException {
 
   }
 
-  public Collection<Vertex> getVertices() {
-    return new Collection<Vertex>();
-  }
+//  public Collection<Vertex> getVertices() {
+//    return new Collection<Vertex>();
+//  }
 
-  public Collection<Edge> getEdges() {
-
-  }
-
-  public Collection<Edge> findShortestPath(long idStartVertex, long idTargetVertex) {
-    // a star here
-  }
+//  public Collection<Edge> getEdges() {
+//
+//  }
+//
+//  public Collection<Edge> findShortestPath(long idStartVertex, long idTargetVertex) {
+//    // a star here
+//  }
 
   public double pathLength(Collection<Edge> path) {
     return 0;
--- src/swe4/tests/TestGraph.java
+++ src/swe4/tests/TestGraph.java
@@ -2,10 +2,12 @@ package swe4.tests;
 
 
 import org.junit.jupiter.api.Test;
+import swe4.gis.Graph;
 
 public class TestGraph {
   @Test
   public void testAddVertex() {
-
+    Graph g = new Graph();
+    g.addVertex(3.4, 3.5);
   }
 }
