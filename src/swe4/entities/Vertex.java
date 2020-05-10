ca2a4d002ea41f848f3c03a5f2a05169fd034545
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-05-10 11:30:38 +0200

    Add id, cost and bestGuess to Vertex class

--- src/swe4/entities/Vertex.java
+++ src/swe4/entities/Vertex.java
@@ -1,12 +1,22 @@
 package swe4.entities;
 
 public class Vertex {
+  private final long id;
   private double x;
   private double y;
+  private double cost;
+  private double bestGuess;
 
-  public Vertex(double x, double y) {
+  public Vertex(double x, double y, long id) {
+    this.id = id;
     this.x = x;
     this.y = y;
+    this.cost = Double.POSITIVE_INFINITY;
+    this.bestGuess = Double.POSITIVE_INFINITY;
+  }
+
+  public long getId() {
+    return id;
   }
 
   public double getX() {
@@ -23,5 +33,25 @@ public class Vertex {
 
   public void setY(double y) {
     this.y = y;
+  }
+
+  public double getCost() {
+    return cost;
+  }
+
+  public void setCost(double cost) {
+    if (cost > 0) {
+      this.cost = cost;
+    }
+  }
+
+  public double getBestGuess() {
+    return bestGuess;
+  }
+
+  public void setBestGuess(double bestGuess) {
+    if (bestGuess >= this.cost) {
+      this.bestGuess = bestGuess;
+    }
   }
 }
