a51d19ecd0007547a309444b8fbe08e3af6303c4
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-05-10 12:42:25 +0200

    Implement Edge class

--- src/swe4/entities/Edge.java
+++ src/swe4/entities/Edge.java
@@ -1,4 +1,31 @@
 package swe4.entities;
 
 public class Edge {
+  private final String name;
+  private final long startVertexId;
+  private final long endVertexId;
+  private final double length;
+
+  public Edge(String name, long startVertexId, long endVertexId, double length) {
+    this.name = name;
+    this.startVertexId = startVertexId;
+    this.endVertexId = endVertexId;
+    this.length = length;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public long getStartVertexId() {
+    return startVertexId;
+  }
+
+  public long getEndVertexId() {
+    return endVertexId;
+  }
+
+  public double getLength() {
+    return length;
+  }
 }
