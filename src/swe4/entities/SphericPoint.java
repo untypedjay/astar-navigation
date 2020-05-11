c53f3f87f64372878c728fb05f2e1422e4415b2f
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-05-11 23:41:10 +0200

    Add SphericPoint class

---
+++ src/swe4/entities/SphericPoint.java
@@ -0,0 +1,35 @@
+package swe4.entities;
+
+public class SphericPoint {
+  private double longitude;
+  private double latitude;
+
+  public SphericPoint() {
+  }
+
+  public SphericPoint(double longitude, double latitude) {
+    this.longitude = longitude;
+    this.latitude = latitude;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(double longitude) {
+    this.longitude = longitude;
+  }
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(double latitude) {
+    this.latitude = latitude;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("(%.5f,%.5f)", longitude, latitude);
+  }
+}
