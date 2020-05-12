95426035de6cf86df032c10f6f457d623d510bf2
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-05-12 21:12:09 +0200

    Add TimeCalculator class

---
+++ src/swe4/gis/TimeCalculator.java
@@ -0,0 +1,17 @@
+package swe4.gis;
+
+import swe4.Edge;
+import swe4.Vertex;
+
+public class TimeCalculator implements CostCalculator {
+
+  @Override
+  public double costs(Edge edge) {
+    return 0;
+  }
+
+  @Override
+  public double estimatedCosts(Vertex v1, Vertex v2) {
+    return 0;
+  }
+}
